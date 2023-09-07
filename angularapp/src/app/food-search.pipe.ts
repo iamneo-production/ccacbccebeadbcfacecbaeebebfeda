6739135c6a72d3f4da34dba9125723df9294428f
Iamneo <techadmin@iamneo.ai>
2023-09-07 14:44:21 +0000

    Backup Commit

--- angularapp/src/app/food-search.pipe.ts
+++ angularapp/src/app/food-search.pipe.ts
@@ -1,21 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'foodSearch'
+  name: 'foodSearch'
 })
 export class FoodSearchPipe implements PipeTransform {
 
-    transform(items: any[], field: string, value: string): any[] {
-        if (!items) {
-            return [];
-        }
-
-        if (!value) {
-            return items;
-        }
-
-        return items.filter(item => item[field].match(new RegExp(value, 'i')));
-    }
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
 
 }
-
